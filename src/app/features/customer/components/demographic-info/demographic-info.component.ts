dbbe4b73092b97b36bd3611c2e4691e871ff7cbb
Mert Erdoğan <nagodretrem@gmail.com>
2024-05-17 11:53:20 +0300

    minor fix

--- src/app/features/customer/components/demographic-info/demographic-info.component.ts
+++ src/app/features/customer/components/demographic-info/demographic-info.component.ts
@@ -94,7 +94,7 @@ export class DemographicInfoComponent implements OnInit {
 
     console.log(individualCustomer);
 
-     /*
+
 
      this.customerApiService.postCustomer(individualCustomer).subscribe({
       next: data => {
@@ -105,7 +105,7 @@ export class DemographicInfoComponent implements OnInit {
       }
     });
 
- */
+
 
     this.store.dispatch(setIndividualCustomer({ individualCustomer }));
     this.router.navigate(['/create/address-info']);
